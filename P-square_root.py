4517dcfccc0a26e155b5576a3fb5e35be2d92bb3
Muzammil950 <muzammil9709@gmail.com>
2025-12-26 23:50:38 +0530

    update programs

---
+++ P-square_root.py
@@ -0,0 +1,7 @@
+ # python progran to find the square root
+
+Num = float(input("ente number : "))
+
+sqr = Num**(0.5) # or 1/2 
+
+print("the square root of num is : ", sqr)
---
+++ math_module/square_root.py
@@ -0,0 +1,10 @@
+# using math module 
+# find square root
+
+
+import math
+num = int(input("enter number is : "))
+
+sqr = math.sqrt(num)
+
+print("the square root of the given number is ",sqr )
