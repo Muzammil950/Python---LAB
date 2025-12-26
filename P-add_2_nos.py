679a66d3cd83010c72dacf9e817a0781eaa469c7
Muzammil950 <muzammil9709@gmail.com>
2025-12-26 23:30:36 +0530

    initialize programs

---
+++ P-add_2_nos.py
@@ -0,0 +1,6 @@
+# Programe to add two numbers in python
+
+num1 = 30
+num2 = 40
+sum = num1 + num2
+print("the sum of two num : ", sum)
---
+++ P-user_input.py
@@ -0,0 +1,8 @@
+# user input method
+
+num1 = float(input("enter num1 : "))
+num2 = float(input("enter num2 : "))
+
+sum = num1 + num2
+
+print("the sum of two num : ", sum)
