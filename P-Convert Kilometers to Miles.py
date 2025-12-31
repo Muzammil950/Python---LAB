08d2796746b3670c1863109ca94cd497344dd1d6
Muzammil950 <muzammil9709@gmail.com>
2025-12-31 12:30:53 +0530

    add a program

---
+++ P-Convert Kilometers to Miles.py
@@ -0,0 +1,11 @@
+# Python Program to Convert Kilometers to Miles
+
+# 1 KELOMETER = 0.621371 MILE
+
+# MILES = 5 * 0.621371
+
+km = float(input("enter your value in kms : "))
+
+miles = (0.621371) * km
+
+print(km,"kms is miles will be ", miles, "miles")
