748ef1c8ea85dac9a1acc0f9a314e5d9b29dc11d
Muzammil950 <muzammil9709@gmail.com>
2026-01-02 11:20:03 +0530

    unlock new P

---
+++ P-Swap_Two_Variables.py
@@ -0,0 +1,15 @@
+# Swap Two Variables in Python
+
+x = 13 
+y = 12
+
+temp = x 
+
+print("the value of temp variables is : ", temp)
+
+x = y
+print("the value of x is : ", x)
+
+
+y = temp
+print("the value of y is : ", y)
