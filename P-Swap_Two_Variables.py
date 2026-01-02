c4dc877dcefb203fade5aba7acfc79178410a459
Muzammil950 <muzammil9709@gmail.com>
2026-01-02 11:25:08 +0530

    adding a Programe

--- P-Swap_Two_Variables.py
+++ P-Swap_Two_Variables.py
@@ -13,3 +13,14 @@ print("the value of x is : ", x)
 
 y = temp
 print("the value of y is : ", y)
+
+
+print("Second method Swap Two Variables in Python ")
+
+x = 13
+y = 12
+
+x,y = y,x
+
+print("the value of x : ", x)
+print("the value of y : ", y)
