a262260f155d280109d09944edb6865e7a6988b4
Muzammil950 <muzammil9709@gmail.com>
2026-01-03 21:11:50 +0530

    add a new P

--- P-Number_Positive_Negative_or_Zero.py
+++ P-Number_Positive_Negative_or_Zero.py
@@ -1,2 +1,13 @@
 # writa a program Number Positive Negative or Zero in python
 
+num = float(input("enter number to check : "))
+
+
+if num > 0 :
+    print("num is positive")
+
+elif num == 0 :
+    print("num is equal to zero")
+
+else :
+    print("num is negative")
