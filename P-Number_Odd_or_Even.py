357d30d6206aa4c29b99ba381c8bf814c55c8f88
Muzammil950 <muzammil9709@gmail.com>
2026-01-03 21:25:20 +0530

    add new Program

---
+++ P-Number_Odd_or_Even.py
@@ -0,0 +1,9 @@
+# W A P Number Odd or Even in python
+
+number = float(input("enter number to check : "))
+
+if number % 2 == 0 :
+    print("the number is Even")
+
+else : 
+    print("the number is Odd")
