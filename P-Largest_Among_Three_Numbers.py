6384fdc096d071039c9dcd34d5fdd0c58e0c5114
Muzammil950 <muzammil9709@gmail.com>
2026-01-06 22:12:59 +0530

    ON-GOING

--- P-Largest_Among_Three_Numbers.py
+++ P-Largest_Among_Three_Numbers.py
@@ -4,3 +4,12 @@ num1 = float(input("Enter the 1st Number : "))
 num2 = float(input("Enter the 2nd Number : "))
 num3 = float(input("Enter the 3rd Number : "))
 
+
+if num1 > num2 and num1 > num3 :
+    print("the num1 is the greatest")
+
+elif num2 > num1 and num2 > num3 :
+    print("the greatest is the num2")
+
+else:
+    print("the greatest is the num3")
