5d58335bd094c2d408f35fa59b9da36e613eb764
Muzammil950 <muzammil9709@gmail.com>
2026-01-14 23:11:17 +0530

    new add a Program

--- P-Sum_of_Natural_Numbers.py
+++ P-Sum_of_Natural_Numbers.py
@@ -3,3 +3,15 @@
 
 num = int(input("Enter number here : "))
 
+
+if num > 0 :
+    print("Please Enter a positive number")
+
+else :
+    sum = 0 
+    while num > 0:
+        sum += num
+        num -= 1
+
+
+    print(sum)
