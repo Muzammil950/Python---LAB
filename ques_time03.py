463be8ce2341a472fba854d817b8552162368e49
Muzammil Shaikh <86058658+Muzammil950@users.noreply.github.com>
2026-06-24 21:53:25 +0530

    Add files via upload

---
+++ ques_time03.py
@@ -0,0 +1,8 @@
+# write a programe to input 2 floating point number  & print their average
+
+a = float(input("enter first num : " ))
+b = float(input("enter sec num : "))
+
+avg = a+b/2
+
+print(avg)
