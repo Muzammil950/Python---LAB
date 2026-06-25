e951cbb7e26753b696894824299813c1bc8cb437
Muzammil Shaikh <86058658+Muzammil950@users.noreply.github.com>
2026-06-25 22:22:50 +0530

    Add files via upload

---
+++ ques_time04.py
@@ -0,0 +1,8 @@
+# write a programme to input 2 int num, a and b 
+# print True if a is grater then or equal to be.
+# if not print false
+
+a = int(input("first num : "))
+b = int(input("sec num : "))
+
+print(a>=b)
